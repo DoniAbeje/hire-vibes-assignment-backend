c2d5b890d4f6a62df898d944d880b0ef78fff3b4
Doni Abeje <doni@peoplise.com>
2022-01-15 21:57:54 +0300

    implement UserService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
 import { UserRepository, IUserRepository } from './user.repository';
+import { IUserService, UserService } from './user.service';
 
 @Module({
   imports: [
@@ -11,6 +12,10 @@ import { UserRepository, IUserRepository } from './user.repository';
     {
       provide: IUserRepository,
       useClass: UserRepository,
+    },
+    {
+      provide: IUserService,
+      useClass: UserService,
     },
   ],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,51 @@
+import {
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from './schema/user.schema';
+import { IUserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
+import { AuthenticateUserDto } from './dto/authenticate-user.dto';
+
+export abstract class IUserService {
+  abstract register(registerUserDto: RegisterUserDto): Promise<User>;
+  abstract authenticate(
+    authenticateUserDto: AuthenticateUserDto,
+  ): Promise<User>;
+}
+
+@Injectable()
+export class UserService implements IUserService {
+  static readonly HASH_ROUNDS = 10;
+  constructor(private userRepo: IUserRepository) {}
+
+  async register(registerUserDto: RegisterUserDto): Promise<User> {
+    const user = await this.userRepo.findByUserName(registerUserDto.userName);
+    const hash = await bcrypt.hash(
+      registerUserDto.password,
+      UserService.HASH_ROUNDS,
+    );
+    if (user) {
+      throw new HttpException('Duplicate userName', 400);
+    }
+    registerUserDto = { ...registerUserDto, password: hash };
+    return this.userRepo.register(registerUserDto);
+  }
+
+  async authenticate(authUserDto: AuthenticateUserDto): Promise<User> {
+    const user = await this.userRepo.findByUserName(authUserDto.userName);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    const checkPassword = await bcrypt.compare(
+      authUserDto.password,
+      user.password,
+    );
+    if (!checkPassword) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
