d0a46149dc6bcaa2660f990910cae4b986116b6c
Doni Abeje <doni@peoplise.com>
2022-01-16 22:49:36 +0300

    import MongooseModule and register FilmRepository

--- src/film/film.module.ts
+++ src/film/film.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FilmRepository, IFilmRepository } from './film.repository';
+import { Film, FilmSchema } from './schema/film.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
+  ],
+  providers: [
+    {
+      provide: IFilmRepository,
+      useClass: FilmRepository,
+    },
+  ],
+})
 export class FilmModule {}
