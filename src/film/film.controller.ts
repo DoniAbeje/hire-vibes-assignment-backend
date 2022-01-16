a3c447eb69e266494f2284a80f8cdce3de184e66
Doni Abeje <doni@peoplise.com>
2022-01-16 23:27:53 +0300

    use JwtGuard on create-film route

--- src/film/film.controller.ts
+++ src/film/film.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtGuard } from '../user/jwt.guard';
 import { CreateFilmDto } from './dto/create-film.dto';
 import { IFilmService } from './film.service';
 
 @Controller('film')
 export class FilmController {
   constructor(private filmService: IFilmService) {}
-
+  
+  @UseGuards(JwtGuard)
+  @ApiBearerAuth()
   @Post('/')
   async create(@Body() createFilmDto: CreateFilmDto) {
     return await this.filmService.create(createFilmDto);
