94af587e11e263949548ec2b74c7497d629dbb0c
Doni Abeje <doni@peoplise.com>
2022-01-16 10:45:36 +0300

    test UserService.register method with duplicate username

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,7 @@
+import { HttpException } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './schema/user.schema';
 import { IUserRepository, UserRepository } from './user.repository';
 import { UserService } from './user.service';
@@ -27,5 +29,14 @@ describe('UserService', () => {
     repo = module.get<IUserRepository>(IUserRepository);
   });
 
-  
+  describe('register', () => {
+    it('should throw HttpException for duplicate username', async () => {
+      const user: User = { username: 'john', password: 'password' };
+      const dto: RegisterUserDto = { username: 'john', password: 'password' };
+      jest.spyOn(repo, 'findByUserName').mockResolvedValue(user);
+
+      const exception = new HttpException('Duplicate userName', 400);
+      await expect(service.register(dto)).rejects.toThrow(exception);
+    });
+  });
 });
