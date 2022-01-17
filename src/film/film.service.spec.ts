4050f102adfe53fe8ad767ee72006ac981c5030d
Doni Abeje <doni@peoplise.com>
2022-01-17 08:34:56 +0300

    test UserService.fetchBySlug method

--- src/film/film.service.spec.ts
+++ src/film/film.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmRepository, IFilmRepository } from './film.repository';
@@ -6,6 +7,20 @@ import { Film } from './schema/film.schema';
 
 describe('FilmService', () => {
   let service: FilmService;
+  let repo: IFilmRepository;
+  const film: Film = {
+    name: 'The Shawshank Redemption (1994)',
+    description:
+      'Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.',
+    releaseDate: new Date('Sep 17 1994'),
+    rating: 4,
+    ticketPrice: 10.99,
+    country: 'USA',
+    genre: Array('Drama'),
+    photo:
+      'https://images-na.ssl-images-amazon.com/images/P/B000P0J0EW.01._SX200_SCLZZZZZZZ_.jpg',
+    slug: 'The-Shawshank-Redemption',
+  } as const;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,9 +38,20 @@ describe('FilmService', () => {
     }).compile();
 
     service = module.get<FilmService>(FilmService);
+    repo = module.get<IFilmRepository>(IFilmRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('fetchBySlug', () => {
+    it('should throw NotFoundException for non existing film with the given slug', async () => {
+      jest.spyOn(repo, 'findBySlug').mockResolvedValue(null);
+      await expect(service.fetchBySlug('non-existing-slug')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should return film instance for existing film with the given slug', async () => {
+      jest.spyOn(repo, 'findBySlug').mockResolvedValue(film);
+      await expect(service.fetchBySlug(film.slug)).resolves.toBe(film);
+    });
   });
 });
