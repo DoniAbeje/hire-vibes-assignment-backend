83b3a32b38e96979cc8b12d937f449ee1c0614a7
Doni Abeje <doni@peoplise.com>
2022-01-19 09:05:57 +0300

    RegisterUserDto.username validation

--- src/user/dto/register-user.dto.ts
+++ src/user/dto/register-user.dto.ts
@@ -1,7 +1,7 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
-  @IsString()
+  @IsNotEmpty()
   readonly username: string;
 
   @MinLength(8)
